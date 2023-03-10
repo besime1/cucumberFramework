e3b5d30434f844bbe82995c7f68111932a6bd6c9
besime1 <bsmdmr86@gmail.com>
2023-03-09 22:05:56 -0500

    Login2Features

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features"   // Specify path from Content Root, features option will establish connection with feature file(s).
         ,glue = "steps"     // specify path from Source Root, glue option will establish connection with step definitions.
-        ,dryRun = false    // if set to True, it will check if ALL step definitions have implementation. Without running your actual scenarios (headless).
+        ,dryRun = false  // if set to True, it will check if ALL step definitions have implementation. Without running your actual scenarios (headless).
         ,monochrome = true    // This will print test results in the console in more human-readable format.
 //        ,tags = "@smoke"       // run test(s) with provided tag name
       //,tags = "@regression or @smoke"       // Run any scenario test that has either regression OR smoke test.  ||
@@ -17,9 +17,6 @@ import org.junit.runner.RunWith;
 
 
 )
-
-
-
 
 public class TestRunner {
 }
--- target/test-classes/features/login.feature
+++ target/test-classes/features/login.feature
@@ -1,20 +1,20 @@
 @login
 Feature:Login tests
+  Background:
+    Given user is on the exelenter webpage
+
   @smoke
   Scenario: Valid user login test
-    Given user is on the exelenter webpage
     When user logs in with valid admin credentials
     Then welcome message is displayed in dashboard page
   @smoke
 
   Scenario: Valid username invalid password test
-    Given user is on the exelenter webpage
     When user pass the valid username and invalid password
     Then login error message for invalid password is displayed
   @regression @smoke
 
   Scenario: Valid username empty password test
-    Given user is on the exelenter webpage
     When user pass the valid username and empty password
     Then login error message for empty password is displayed
 
